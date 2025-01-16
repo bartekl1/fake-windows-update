b6dd29f6619a484a16aac2693af8a34e1f7b9620
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2025-01-16 18:32:29 +0100

    Update project configuration and improve code structure

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow } = require("electron");
 const child_process = require("child_process");
-const path = require('path');
+const path = require("path");
 
 const createWindow = () => {
     const mainWindow = new BrowserWindow({
@@ -15,18 +15,17 @@ const createWindow = () => {
     });
 
     mainWindow.setMenuBarVisibility(false);
-    // mainWindow.setFullScreen(true);
     mainWindow.setAlwaysOnTop(true, "screen-saver");
 
     mainWindow.loadFile("win10.html");
 
     mainWindow.on("blur", () => {
-        mainWindow.focus(); // Przywróć focus, jeśli okno straci uwagę
+        mainWindow.focus();
     });
 
     setInterval(() => {
         mainWindow.setAlwaysOnTop(true, "screen-saver");
-    }, 1000); // Regularnie odświeża stan "always on top"
+    }, 1000);
 
     // mainWindow.webContents.openDevTools()
 };
@@ -34,8 +33,7 @@ const createWindow = () => {
 app.whenReady().then(() => {
     createWindow();
 
-    // const block_input_exe_path = path.join(process.resourcesPath, "assets", 'block_input.exe');
-    const block_input_exe_path = path.join(app.getAppPath(), "assets", 'block_input.exe');
+    const block_input_exe_path = path.join(app.getAppPath(), "block_input.exe");
     child_process.spawn(block_input_exe_path, {
         detached: true,
         stdio: "ignore",
